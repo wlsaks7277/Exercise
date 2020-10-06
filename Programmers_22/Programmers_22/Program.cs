6773b04cfdde3d848c5cf74039610c739fc28283
gittest01 <dkfkqldk90@naver.com>
2020-10-06 21:57:28 +0900

    Programmers_3day

---
+++ Programmers_22/Programmers_22/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * 문제 설명
+함수 solution은 정수 n을 매개변수로 입력받습니다.
+n의 각 자릿수를 큰것부터 작은 순으로 정렬한 새로운 정수를 리턴해주세요. 
+예를들어 n이 118372면 873211을 리턴하면 됩니다.
+
+제한 조건
+n은 1이상 8000000000 이하인 자연수입니다.
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programmers_22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new Solution();
+            Console.WriteLine(s.solution(12124124));
+        }
+    }
+
+    public class Solution
+    {
+        public long solution(long n)
+        {
+            long answer = 0;
+
+            char[] c = n.ToString().ToCharArray();
+
+            //Array 활용 하는 법
+            Array.Sort(c);
+            Array.Reverse(c);
+            answer = long.Parse(new string(c));
+
+            return answer;
+        }
+    }
+}
---
+++ Programmers_23/Programmers_23/Program.cs
@@ -0,0 +1,47 @@
+﻿/*
+ * 문제 설명
+임의의 양의 정수 n에 대해, n이 어떤 양의 정수 x의 제곱인지 아닌지 판단하려 합니다.
+n이 양의 정수 x의 제곱이라면 x+1의 제곱을 리턴하고,
+n이 양의 정수 x의 제곱이 아니라면 -1을 리턴하는 함수를 완성하세요.
+
+제한 사항
+n은 1이상, 50000000000000 이하인 양의 정수입니다.
+입출력 예
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programmers_23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new Solution();
+            Console.WriteLine(s.solution(169));
+        }
+    }
+
+    public class Solution
+    {
+        public long solution(long n)
+        {
+            long answer = 0;
+            long val = 0;
+            for (int i=1; i<n+1; i++)
+            {
+                val = n / i;
+                if(val*val==n)
+                {
+                    answer = val+1;
+                    answer *= answer;
+                }
+            }
+            if (answer == 0) return -1;
+            return answer;
+        }
+    }
+}
